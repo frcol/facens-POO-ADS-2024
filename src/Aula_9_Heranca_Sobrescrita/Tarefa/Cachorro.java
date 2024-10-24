f49108b98c5207eab0761b735e9a079ea19b8ea2
Fabio Colombini <fabiorcolombini@gmail.com>
2024-10-24 10:11:20 -0300

    atualizado

---
+++ src/Aula_9_Heranca_Sobrescrita/Tarefa/Cachorro.java
@@ -0,0 +1,36 @@
+package Aula_9_Heranca_Sobrescrita.Tarefa;
+
+public class Cachorro extends Animal {
+    private int velocidade;
+
+    // ==========================================
+    public Cachorro() {
+
+    }
+    
+    public Cachorro(String nome, int idade, int velocidade) {
+        super(nome, idade);
+        this.velocidade = velocidade;
+    }
+
+    // ==========================================
+    public int getVelocidade() {
+        return velocidade;
+    }
+
+    public void setVelocidade(int velocidade) {
+        this.velocidade = velocidade;
+    }
+
+    // ==========================================
+    @Override
+    public String fala() {
+        return "Auauauauauuauau";
+    }
+
+    @Override
+    public String imprimir() {
+        return super.imprimir()
+                +"\nVelocidade: "+velocidade;
+    } 
+}
---
+++ src/Aula_9_Heranca_Sobrescrita/Tarefa/Capivara.java
@@ -0,0 +1,33 @@
+package Aula_9_Heranca_Sobrescrita.Tarefa;
+
+
+public class Capivara extends Animal{
+    private int peso;
+
+    public Capivara() {
+    }
+
+    public Capivara(String nome, int peso, int idade) {
+        super(nome, idade);
+        this.peso = peso;
+    }
+
+    public int getPeso() {
+        return peso;
+    }
+
+    public void setPeso(int peso) {
+        this.peso = peso;
+    }
+
+    @Override
+    public String fala() {
+        return "Capicara falando....";
+    }
+
+    @Override
+    public String imprimir() {
+        return super.imprimir()
+                +"\nPeso: "+peso;
+    }
+}
